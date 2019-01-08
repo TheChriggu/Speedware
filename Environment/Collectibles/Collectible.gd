aaa15ce894aaa736868b17878c254b300ff07be2
ChristianHeusser <none@none>
2019-01-08 16:43:08 +0100

    -added level to test speed boosters
    --HG--
    branch : Speedboost_Camera

--- Environment/Collectibles/Collectible.gd
+++ Environment/Collectibles/Collectible.gd
@@ -1,6 +1,8 @@
 extends Sprite
 
+signal collected
 func _on_Area2D_area_entered(area):
 	$AnimationPlayer.play("Collectible_collected")
 	#$CollectiblesCollected.CollectiblesCollected()
-	get_parent().get_node("UI/CollectiblesCollected").CollectiblesCollected()
+	#get_parent().get_node("UI/CollectiblesCollected").CollectiblesCollected()
+	emit_signal("collected")

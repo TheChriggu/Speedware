2dcb454cfa4d5328409f154bf5d427dedc81e50e
ChristianHeusser <none@none>
2019-01-08 12:21:02 +0100

    -changed speedboost, to contain all necessary variables in itself -speedboost calls level head node
    --HG--
    branch : Speedboost_Camera

--- Environment/Speedboost/Speedboost.gd
+++ Environment/Speedboost/Speedboost.gd
@@ -1,5 +1,22 @@
 extends Sprite
 
+export var NEW_MAX_SPEED = 1800
+export var NEW_MAX_FORCE = 1900
+var old_max_speed = 1000
+var old_max_force = 1500
+
+signal speedboost_start
+signal speedboost_mid
+signal speedboost_stop
+
 func _on_Area2D_area_entered(area):
 	$AnimationPlayer.play("SpeedboostCollected")
-	get_parent().get_node("player/AnimationPlayer").play("speedboost")
+
+func start():
+	emit_signal("speedboost_start", NEW_MAX_SPEED, NEW_MAX_FORCE)
+
+func mid():
+	emit_signal("speedboost_mid")
+
+func stop():
+	emit_signal("speedboost_stop", old_max_speed, old_max_force)
--- Level.gd
+++ Level.gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _ready():
+	##TODO: Set old max speed and old max force in Speedbooster
 	$ForwardLookingCamera.WALK_MAX_SPEED = $player.WALK_MAX_SPEED
 
 func _physics_process(delta):
@@ -12,3 +13,20 @@ func _on_player_switched_color_to_orange():
 
 func _on_player_switched_color_to_purple():
 	$Lasers.switch_to_purple()
+
+
+func _on_Speedboost_speedboost_start(new_max_speed, new_max_force):
+	$player.WALK_MAX_SPEED = new_max_speed
+	$player.WALK_FORCE = new_max_force
+
+
+
+
+func _on_Speedboost_speedboost_mid():
+	##TODO: return Camera
+	pass # replace with function body
+
+
+func _on_Speedboost_speedboost_stop(new_max_speed, new_max_force):
+	$player.WALK_MAX_SPEED = new_max_speed
+	$player.WALK_FORCE = new_max_force

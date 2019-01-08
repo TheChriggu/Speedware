a6e6a43f03c5b18ece3c90bb60f2d2b5c1deb4f3
ChristianHeusser <none@none>
2019-01-08 12:50:57 +0100

    Made camera move behind player, while on speed boost
    --HG--
    branch : Speedboost_Camera

--- Camera/ForwardLookingCamera.gd
+++ Camera/ForwardLookingCamera.gd
@@ -10,13 +10,13 @@ var fixedCamera = false #bool to track weather the camera is currently fixed to 
 
 var WALK_MAX_SPEED = 0
 
-#Camera calculation
+#Regular camera calculation
 func update_camera(player_position, velocity):
 	$AverageSpeedCalculation.addNew(velocity)
 	
 	#offset is proportional to current movement speed/max speed
 	var next_offset_h = MAX_CAM_DISTANCE_H*$AverageSpeedCalculation.average().x/WALK_MAX_SPEED
-	next_offset_h = smoothMovement(next_offset_h, last_offset.x, MAX_CAM_SPEED_H)
+	next_offset_h = clampSpeed(next_offset_h, last_offset.x, MAX_CAM_SPEED_H)
 	last_offset.x = next_offset_h
 	
 	#send new offset to camera
@@ -31,13 +31,40 @@ func update_camera(player_position, velocity):
 	else:
 		next_offset_v = player_position.y
 		
-	next_offset_v = smoothMovement(next_offset_v, last_offset.y, MAX_CAM_SPEED_V)
+	next_offset_v = clampSpeed(next_offset_v, last_offset.y, MAX_CAM_SPEED_V)
 	last_offset.y = next_offset_v
 	
 	#send new offset to camera
 	$FocalPoint.setVertical(next_offset_v)
 
-func smoothMovement(var next_position, var last_offset, var max_speed):
+#Camera calculation during speed boost
+func update_camera_speed_boost(player_position, velocity):
+	$AverageSpeedCalculation.addNew(velocity)
+	
+	#offset is proportional to current movement speed/max speed
+	var next_offset_h = -MAX_CAM_DISTANCE_H*$AverageSpeedCalculation.average().x/WALK_MAX_SPEED
+	next_offset_h = clampSpeed(next_offset_h, last_offset.x, MAX_CAM_SPEED_H)
+	last_offset.x = next_offset_h
+	
+	#send new offset to camera
+	$FocalPoint.setHorizontal(player_position.x + next_offset_h)
+	
+	#vertical cam offset calculation
+	#vertical cam position
+	var next_offset_v #vertical offset of the camera
+	#set new camera offset, to the anchor if camera is free, otherwise follow the player
+	if $HeadCamCollider.FIXED_CAMERA:
+		next_offset_v = $HeadCamCollider.CURRENT_ANCHOR #camera offset in relation to the player
+	else:
+		next_offset_v = player_position.y
+		
+	next_offset_v = clampSpeed(next_offset_v, last_offset.y, MAX_CAM_SPEED_V)
+	last_offset.y = next_offset_v
+	
+	#send new offset to camera
+	$FocalPoint.setVertical(next_offset_v)
+	
+func clampSpeed(var next_position, var last_offset, var max_speed):
 	#if camera speed is too high (new position is further away than allowed), set the camera to max speed
 	if (next_position - last_offset) > max_speed: #when moving downwards
 		next_position = last_offset + max_speed

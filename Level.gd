cd726b4c0572e81406a5d65e01a8fe4cd2632768
ChristianHeusser <none@none>
2019-01-09 17:18:48 +0100

    -Made a mistake previously, and forgot to add the new files
    -Took all UI elements out of player, and implemented them in the default level
    
    --HG--
    branch : Fruehjahrsputz

--- Level.gd
+++ Level.gd
@@ -3,22 +3,24 @@ extends Node
 var player_is_in_speed_boost = false
 
 func _ready():
-	$ForwardLookingCamera.WALK_MAX_SPEED = $player.WALK_MAX_SPEED
+	$Player.get_node("ForwardLookingCamera").WALK_MAX_SPEED = $Player.get_node("player").WALK_MAX_SPEED
 
 func _physics_process(delta):
 	if player_is_in_speed_boost:
-		$ForwardLookingCamera.update_camera_speed_boost($player.position, $player.velocity)
+		$Player.get_node("ForwardLookingCamera").update_camera_speed_boost($Player.get_node("player").position, $Player.get_node("player").velocity)
 	else:
-		$ForwardLookingCamera.update_camera($player.position, $player.velocity)
+		$Player.get_node("ForwardLookingCamera").update_camera($Player.get_node("player").position, $Player.get_node("player").velocity)
 
 func _on_player_switched_color_to_orange():
-	$Lasers.switch_to_orange()
+	$Level.get_node("Lasers").switch_to_orange()
+	$UI.get_node("ColorIndicator").SwitchToOrange()
 
 func _on_player_switched_color_to_purple():
-	$Lasers.switch_to_purple()
+	$Level.get_node("Lasers").switch_to_purple()
+	$UI.get_node("ColorIndicator").SwitchToPurple()
 
 func _on_Speedboost_speedboost_start(speed):
-	$player.velocity = speed
+	$Player.get_node("player").velocity = speed
 	player_is_in_speed_boost = true
 
 func _on_Speedboost_speedboost_stop():
@@ -26,11 +28,11 @@ func _on_Speedboost_speedboost_stop():
 
 var is_in_fullspeed_mode = false
 func _process(delta):
-	if $player.velocity.x >= $player.WALK_MAX_SPEED - 100 && !is_in_fullspeed_mode:
-		$AnimationPlayer.play("Fullspeed Mode")
+	if $Player.get_node("player").velocity.x >= $Player.get_node("player").WALK_MAX_SPEED - 100 && !is_in_fullspeed_mode:
+		$FulspeedMode.get_node("AnimationPlayer").play("Fullspeed Mode")
 		is_in_fullspeed_mode = true
 	
-	if $player.velocity.x < $player.WALK_MAX_SPEED - 100 && is_in_fullspeed_mode:
-		$AnimationPlayer.stop()
-		$AnimationPlayer.play("Fullspeed Mode Stop")
+	if $Player.get_node("player").velocity.x < $Player.get_node("player").WALK_MAX_SPEED - 100 && is_in_fullspeed_mode:
+		$FulspeedMode.get_node("AnimationPlayer").stop()
+		$FulspeedMode.get_node("AnimationPlayer").play("Fullspeed Mode Stop")
 		is_in_fullspeed_mode = false

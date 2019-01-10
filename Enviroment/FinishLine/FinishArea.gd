1ba325639f52041890376a4764c14f0dc306d215
ChristianHeusser <none@none>
2019-01-10 17:29:38 +0100

    -finish line was stopping sound in parent. Removed that line
    --HG--
    branch : Fruehjahrsputz

--- Enviroment/FinishLine/FinishArea.gd
+++ Enviroment/FinishLine/FinishArea.gd
@@ -6,7 +6,6 @@ func _on_Area2D_area_entered(area):
 	emit_signal("finish_line_passed")
 	$AnimationPlayer.play("FinishLineassed")
 	$LinePassedSoundEffect.playing = true
-	get_parent().get_node("BackgroundMusic").playing = false
 
 func _on_LinePassedSoundEffect_finished():
 	$LinePassedMusic.playing = true

ba0c86188b5fb73a2f2ca54db34fe84812518fbe
ChristianHeusser <none@none>
2019-01-10 13:24:52 +0100

    -cleaned the cam colliders
    --HG--
    branch : Fruehjahrsputz

--- Camera/CamCollider.gd
+++ Camera/CamCollider.gd
@@ -4,8 +4,8 @@ extends Area2D
 export var FIXED_CAMERA = true #tracker to check if the collider is supposed to fix camera to a certain hight, or to free it
 
 
-signal newCamera(anchor, fixed)
+signal new_camera(anchor, fixed)
 
 #if player hits the collider, send a signal for the camera
 func _on_CamCollider_body_entered(body):
-	emit_signal("newCamera", position.y, FIXED_CAMERA)
+	emit_signal("new_camera", position.y, FIXED_CAMERA)

777bcb8455cba2c26cea4c9eeea62b3e202e677f
christian heusser <none@none>
2019-01-10 19:08:58 +0100

    -Set Camera a bit higher and further left -made return to title screen button work -Bug: Buttons in title screen are moving around randomly
    --HG--
    branch : Fruehjahrsputz

--- Camera/FocalPoint.gd
+++ Camera/FocalPoint.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-var horizontalDistance = 500
-var verticalDistance = 200
+export var HORIZONTAL_OFFSET = 400
+export var VERTICAL_OFFSET = 100
 
 #set the horizontal camera position
 func SetHorizontal(offset):
-	global_position.x = offset + horizontalDistance
+	global_position.x = offset + HORIZONTAL_OFFSET
 
 #set the vertical camera position
 func SetVertical(offset):
-	global_position.y  = offset - verticalDistance
+	global_position.y  = offset - VERTICAL_OFFSET
--- Scenes/Pause Menu/PauseMenu_return_to_Title.gd
+++ Scenes/Pause Menu/PauseMenu_return_to_Title.gd
@@ -2,5 +2,5 @@ extends Node
 
 func _on_Button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene("res://Menus/Startmenu/TitleScreen.tscn")
+	get_tree().change_scene("res://Scenes/Main Menu/TitleScreen.tscn")
 	

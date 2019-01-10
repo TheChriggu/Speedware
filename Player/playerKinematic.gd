1f897570834e382ffbbbbb53ff5ed6a785faa545
ChristianHeusser <none@none>
2019-01-10 11:43:25 +0100

    Overhauled player script: -renamed all variables, functions, animation names and signal names to comply with consistent nomenclature -deleted several redundant variables and animations -moved most things in their own function for higher readability
    --HG--
    branch : Fruehjahrsputz

--- Player/playerKinematic.gd
+++ Player/playerKinematic.gd
@@ -1,36 +1,122 @@
 extends KinematicBody2D
-#This is the Script for the Character
 
 #Variables
 export var GRAVITY = 3000.0 #pixels/second/second
-#Movementspeed
-export var WALK_FORCE = 1500
-#Minimum Speed to move the Char
-export var WALK_MIN_SPEED = 300
-#Maximum Speed for the Char
-export var WALK_MAX_SPEED = 1000
-export var STOP_FORCE = 1300
-export var JUMP_SPEED = 1200
-#Time the player can jump after leaving an edge (Double click for double jump mechanic~)
-export var JUMP_MAX_AIRBORNE_TIME = 0.001
+export var WALK_FORCE = 1500 #Movement accelleration
+export var WALK_MIN_SPEED = 300 #Minimum speed at which the character moves
+export var WALK_MAX_SPEED = 1000 #Maximum speed at which the character moves
+export var STOP_FORCE = 1300 #Level of friction, which slows the player down
+export var JUMP_SPEED = 1200 #Speed to which the players vertical velocity gets set on a jump. Influences jump height
+export var JUMP_MAX_AIRBORNE_TIME = 0.001 #Time the player can jump after leaving an edge (Double click for double jump mechanic~)
 
-export var SLIDE_STOP_VELOCITY = 20.0 # one pixel/second
-var jump_cut_velocity = 200 #Value used for the jump() and jump_cut() function
-var velocity = Vector2()
-export var ON_AIR_TIME = 2
-var jumping = false
+var jumpCutVelocity = 200 #vertical speed, which the jump cannot go over
+var velocity = Vector2() #Vector containing the horizontal & vertical player speed
+export var onAirTime = 0 #Time the player has spent in the air
+var isJumping = false #Boolean to track weather or not player is jumping
 
-var prev_jump_pressed = false
-export var isGreen = false
+export var IS_ORANGE = false #Boolean to track, if the player ic currently orange
 
-signal switched_color_to_purple
-signal switched_color_to_orange
+signal switched_color_to_purple #signal to send if the player has switched his color to orange
+signal switched_color_to_orange #signal to send if the player has switched his color to purple
 
+var isLeaningLeft = false
+var isLeaningRight = false
+
+func _physics_process(delta):
+	HorizontalMovement(delta)
+	VerticalMovement(delta)
+	UIInteraction()
+
+func HorizontalMovement(delta):
+	var force = Vector2(0, GRAVITY) #Vector containing the forces which get applied to the player every frame
+	force.x = LeftForce() + RightForce() #calculate horizontal forces
+	
+	#if forces are applying, play their animtaions, otherwise apply friction
+	if force.x > 0:
+		LeanRight()
+	elif force.x < 0:
+		LeanLeft()
+	else:
+		ApplyFriction(delta)
+	
+	# Integrate forces to velocity
+	velocity += force *delta
+	
+	# Integrate velocity into motion and move
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+func VerticalMovement(delta):
+	if is_on_floor():
+		onAirTime = 0
+		
+	else:
+		onAirTime += delta
+	
+	Jump()
+
+func LeftForce():
+	if Input.is_action_pressed("move_left"):
+		if velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED:
+			return -WALK_FORCE
+	
+	return 0
+
+func RightForce():
+	if Input.is_action_pressed("move_right"):
+		if velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED:
+			return WALK_FORCE
+	
+	return 0
+
+func ApplyFriction(delta):
+	var vsign = sign(velocity.x)
+	var vlen = abs(velocity.x)
+	vlen -= STOP_FORCE * delta
+	if vlen < 0:
+		vlen = 0
+	
+	velocity.x = vlen * vsign
+
+func Jump():
+	if Input.is_action_just_pressed("jump") and not isJumping and onAirTime < JUMP_MAX_AIRBORNE_TIME:
+		#Player can jump, even after leaving the Edge for some time
+		velocity.y = -JUMP_SPEED
+		isJumping = true
+		
+	if Input.is_action_just_released("jump"):
+		JumpCut()
+	
+	if isJumping and velocity.y > 0:
+		isJumping = false
+
+#Cut the jump, when a certain speed is reached
+func JumpCut():
+    if velocity.y < -jumpCutVelocity:
+        velocity.y = -jumpCutVelocity
+
+func LeanLeft():
+	if !isLeaningLeft:
+		$AnimationPlayer.play("LeanLeft")
+		isLeaningLeft = true
+		isLeaningRight = false
+
+func LeanRight():
+	if !isLeaningRight:
+		$AnimationPlayer.play("LeanRight")
+		isLeaningRight = true
+		isLeaningLeft = false
+
+func UIInteraction():
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	
+	if Input.is_action_just_pressed("switchColor"):
+			SwitchColor()
 
 #Colorswitch Mechanic
 func SwitchColor():
-	isGreen = not isGreen
-	if isGreen:
+	IS_ORANGE = not IS_ORANGE
+	if IS_ORANGE:
 		$AnimationPlayer.play("SwitchPurpleToOrange")
 		$ColorswitchSound.playing = true
 		emit_signal("switched_color_to_orange")
@@ -38,113 +124,3 @@ func SwitchColor():
 		$AnimationPlayer.play("SwitchOrangeToPurple")
 		$ColorswitchSound.playing = true
 		emit_signal("switched_color_to_purple")
-
-#Jump() and jump_cut() are for the "Mario-like"-jump Height controls (MLJ=Mario-like Jump)
-func jump():
-    velocity.y = -JUMP_SPEED
-func jump_cut():
-    if velocity.y < -jump_cut_velocity:
-        velocity.y = -jump_cut_velocity
-
-func moveRight():
-	pass
-	#$AnimationPlayer.play("Move_Right")
-func _physics_process(delta):
-	#basic Player movement
-	var force = Vector2(0, GRAVITY)
-	var walk_left = Input.is_action_pressed("move_left")
-	var walk_right = Input.is_action_pressed("move_right")
-	var speedboost = Input.is_action_just_pressed("speedboost")
-	var speedboostOff = Input.is_action_just_pressed("speedboostOff")
-	var jump = Input.is_action_pressed("jump")
-	#MLJ
-	if Input.is_action_just_pressed("jump") && velocity.y < -500:
-		velocity.y = -500
-		jump()
-	#MLJ
-	if Input.is_action_just_released("jump"):
-		jump_cut()
-	if Input.is_action_just_pressed("restart"):
-		get_tree().reload_current_scene()
-	var stop = true
-	
-	if speedboost:
-		$AnimationPlayer.play("speedboost")
-#		force.x += WALK_FORCE
-#	if speedboostOff:
-#		WALK_MAX_SPEED = 1000
-	
-	if walk_left:
-		lean_left()
-		if velocity.x <= WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED:
-			force.x -= WALK_FORCE
-			stop = false
-			
-	elif walk_right:
-		lean_right()
-		#$AnimationPlayer.play("Move_Right")
-		if velocity.x >= -WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED:
-			force.x += WALK_FORCE
-			stop = false
-			
-	
-	if stop:
-		var vsign = sign(velocity.x)
-		var vlen = abs(velocity.x)
-		vlen -= STOP_FORCE * delta
-		if vlen < 0:
-			vlen = 0
-		
-		velocity.x = vlen * vsign
-	
-	
-	
-	
-	
-	# Integrate forces to velocity
-	velocity += force *delta
-	
-	
-	# Integrate velocity into motion and move
-	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
-	if is_on_floor():
-		ON_AIR_TIME = 0
-		
-	if jumping and velocity.y > 0:
-		# If falling, no longer jumping
-		jumping = false
-	
-	if ON_AIR_TIME < JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping:
-		#Player can jump, even after leaving the Edge for some time
-		velocity.y = -JUMP_SPEED
-		jumping = true
-
-	
-	ON_AIR_TIME += delta
-	prev_jump_pressed = jump
-	#Colorswitch Mechanic
-	if Input.is_action_just_pressed("switchColor"):
-			SwitchColor()
-	if Input.is_action_just_pressed("restart"):
-		get_tree().reload_current_scene()
-	
-
-var is_leaning_left = false
-var is_leaning_right = false
-
-func lean_left():
-	if is_leaning_right:
-		$AnimationPlayer.play("Move_Stop")
-		is_leaning_right = false
-	if !is_leaning_left:
-		$AnimationPlayer.play("Move_Left")
-		is_leaning_left = true
-
-func lean_right():
-	if is_leaning_left:
-		$AnimationPlayer.play("Move_Stop")
-		is_leaning_left = false
-	if !is_leaning_right:
-		$AnimationPlayer.play("Move_Right")
-		is_leaning_right = true

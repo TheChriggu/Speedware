76d6189a2fe48e523ebb880ad82e13f5ce653485
ChristianHeusser <none@none>
2019-01-10 17:09:18 +0100

    -Moved everything to new folder structure
    --HG--
    branch : Fruehjahrsputz

--- Fruehjahrsputz/Everything/Scenes/Pause Menu/Pause.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-#This Script is used to Pause the Game
-func _input(event):
-	if event.is_action_pressed("pause"):
-		var newPauseState = not get_tree().paused
-		get_tree().paused = newPauseState
-		visible = newPauseState

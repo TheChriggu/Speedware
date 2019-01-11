7ce2d05cd6995e3dd5b0c6814b099c63a0e0fa56
ChristianHeusser <none@none>
2019-01-11 13:41:58 +0100

    Made Timed Speedbooster work
    --HG--
    branch : Fruehjahrsputz

--- Enviroment/Speedboost/Speedboost.gd
+++ Enviroment/Speedboost/Speedboost.gd
@@ -6,14 +6,15 @@ export var LENGTH = 1.5
 signal speedboost_start
 signal speedboost_stop
 
-#func _ready():
-#	$AnimationPlayer.NewStop()
+func _ready():
+	$Timer.wait_time = LENGTH
 
 func _on_Area2D_area_entered(area):
 	#stop a previous animation from playing before starting this one
 	$AnimationPlayer.stop()
 	$AnimationPlayer.play("SpeedboostCollected")
+	$Timer.start()
 	emit_signal("speedboost_start", Vector2(SPEED, 0).rotated(rotation))
 
-func Stop():
+func _on_Timer_timeout():
 	emit_signal("speedboost_stop")

911059276c81274e35a8787c7d44dd1b5b5f2c5d
BryanPrintz <none@none>
2019-02-04 15:21:13 +0100

    Added: Removed: Fixed: the bug from the earlier version is fixed Bugs:

--- Enviroment/FinishLine/BackToMenuButton.gd
+++ Enviroment/FinishLine/BackToMenuButton.gd
@@ -4,3 +4,4 @@ extends TextureButton
 
 func _on_BackToMenu_pressed():
 	get_tree().change_scene("res://Scenes/Main Menu/TitleScreen.tscn")
+	get_tree().paused=false
--- Enviroment/FinishLine/NextLevelButton.gd
+++ Enviroment/FinishLine/NextLevelButton.gd
@@ -4,4 +4,5 @@ export(PackedScene) var scene_to_load
 
 func _on_NextLevel_pressed():
 	get_tree().change_scene_to(scene_to_load)
+	get_tree().paused =false
 

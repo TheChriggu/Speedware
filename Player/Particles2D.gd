0ddeca8e82a092f33482ee3ccf571b23dde7a06b
ChristianHeusser <none@none>
2019-02-06 16:57:13 +0100

    Fixed: -Overlaying particles during color switch are now possible

---
+++ Player/Particles2D.gd
@@ -0,0 +1,32 @@
+extends Particles2D
+
+var mustSwitchColorToPurple = false
+var mustSwitchColorToOrange = false
+
+#Stop emitting when time runs out, then check, which color needs to be changed
+func _on_Timer_timeout():
+	emitting = false
+	if mustSwitchColorToPurple:
+		OrangeToPurple()
+	if mustSwitchColorToOrange:
+		PurpleToOrange()
+
+func SetTimerTime(time):
+	$Timer.wait_time = time
+
+func StartTimer():
+	$Timer.start()
+
+func OrangeToPurple():
+	if !emitting:
+		process_material = load("res://Player/Particles/PurpleSwitcheffectExplosion.tres")
+		mustSwitchColorToPurple = false
+	else:
+		mustSwitchColorToPurple = true
+		
+func PurpleToOrange():
+	if !emitting:
+		process_material = load("res://Player/Particles/OrangeSwitcheffectExplosion.tres")
+		mustSwitchColorToOrange = false
+	else:
+		mustSwitchColorToOrange = true

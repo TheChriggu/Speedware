b72b67d0b48161637f7a31ff002efd8c69d18679
ChristianHeusser <none@none>
2019-02-13 19:49:07 +0100

    Fixed: -Player Can now switch color again
    --HG--
    branch : Collider Cleanup

--- Player/Player Animation/Base.gd
+++ Player/Player Animation/Base.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 func Run():
 	$AnimationPlayer.play("Run")
+
+func SwitchColorToOrange():
+	$ColorSwitchAnimation.play("SwitchColorToOrange")
+
+func SwitchColorToPurple():
+	$ColorSwitchAnimation.play("SwitchColorToPurple")

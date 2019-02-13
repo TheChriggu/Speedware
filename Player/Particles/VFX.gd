155111aaef65ec9a913ca546ca1e4b40af4588e0
ChristianHeusser <none@none>
2019-02-13 21:37:22 +0100

    Fixed: Trail Particle Effect now working as intended
    --HG--
    branch : Collider Cleanup

--- Player/Particles/VFX.gd
+++ Player/Particles/VFX.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 func PlayJumpAnimation():
-	$AnimationPlayer.play("JumpAnimation")
+	#$AnimationPlayer.play("JumpAnimation")
+	pass
 
 func TurnBoostParticlesOn():
 	$ParticlesIfBoosted.emitting = true
+	$ParticlesIfBoosted2.emitting = true
 
 func TurnBoostParticlesOff():
 	$ParticlesIfBoosted.emitting = false
+	$ParticlesIfBoosted2.emitting = false

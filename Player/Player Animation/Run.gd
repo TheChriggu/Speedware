81be6b5338df125a5346a61ad090d358d669f60b
ChristianHeusser <none@none>
2019-02-15 13:27:03 +0100

    Added: -Implementation and blending of animations
    --HG--
    branch : RunningAnimation

--- Player/Player Animation/Run.gd
+++ Player/Player Animation/Run.gd
@@ -1,13 +1,53 @@
 extends Node2D
 
 var isRunning =  false
-func Run():
+var isJumpingUp = false
+var isJumpingDown = false
+var isIdle = false
+
+func SetAllVariablesToFalse():
+	isRunning = false
+	isJumpingUp = false
+	isJumpingDown = false
+	isIdle = false
+	
+func Run(speed):
 	if !isRunning:
-		$Base.Run()
+		$Base.Run(ClampRunningSpeed(speed))
+		SetAllVariablesToFalse()
 		isRunning = true
+	else:
+		$Base.SetAnimationSpeed(ClampRunningSpeed(speed))
 
 func SwitchColorToOrange():
 	$Base.SwitchColorToOrange()
 
 func SwitchColorToPurple():
 	$Base.SwitchColorToPurple()
+
+func JumpUp():
+	if !isJumpingUp:
+		$Base.JumpUp()
+		SetAllVariablesToFalse()
+		isJumpingUp = true
+
+func JumpDown():
+	if !isJumpingDown:
+		$Base.JumpDown()
+		SetAllVariablesToFalse()
+		isJumpingDown = true
+
+func Idle():
+	if !isIdle:
+		$Base.Idle()
+		SetAllVariablesToFalse()
+		isIdle = true
+
+var minSpeed = 0.8
+var maxSpeed = 2
+func ClampRunningSpeed(speed):
+	if speed < minSpeed:
+		return minSpeed
+	elif speed > maxSpeed:
+		return maxSpeed
+	return speed

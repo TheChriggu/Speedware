ead944a8ff704581d4ae82f41933f3754c140a7f
BryanPrintz <none@none>
2019-02-22 11:39:21 +0100

    Added: Game now also saves star rating of the player to make it easier to implement in the level selection

--- Enviroment/FinishLine/FinishArea.gd
+++ Enviroment/FinishLine/FinishArea.gd
@@ -5,7 +5,12 @@ export var PossibleLevelBestTime = 00.00
 export var TwoStarRatingTolerance = 8
 export var OneStarRatingTolerance = 15
 export var ZeroStarRatingTolerance = 20
+var config = ConfigFile.new()
+var Levelnumber
 
+func _ready():
+	Levelnumber = get_parent().get_parent().get_node("UI/Timer/Levelnumber").text
+	print(Levelnumber)
 func _on_Area2D_area_entered(area):
 	emit_signal("finish_line_passed")
 	$LinePassedSoundEffect.playing = true
@@ -21,15 +26,38 @@ func _on_NextLevel_pressed():
 
 func _on_player_FinishLineAnimationFinished():
 	$AnimationPlayer.play("FinishLinePassed")
-
+	config.load("user://settings.cfg")
+	var PreviousStarRating = config.get_value("StarRating", Levelnumber,0)
+	
+	var StarRating = 0
+	
 	if getFinishTime() > PossibleLevelBestTime && getFinishTime() < (PossibleLevelBestTime+TwoStarRatingTolerance):
 		$Popup/Control/PopupBackground/AnimationPlayer.play("2StarRating")
+		StarRating = 2
+		if PreviousStarRating < StarRating:
+			setStarRating(StarRating)
+		config.save("user://settings.cfg")
+		
 	elif getFinishTime() > (PossibleLevelBestTime+TwoStarRatingTolerance) && getFinishTime() < (PossibleLevelBestTime+OneStarRatingTolerance):
 		$Popup/Control/PopupBackground/AnimationPlayer.play("1StarRating")
+		StarRating = 1
+		if PreviousStarRating < StarRating:
+			setStarRating(StarRating)
+		config.save("user://settings.cfg")
+		
 	elif getFinishTime() > (PossibleLevelBestTime+OneStarRatingTolerance) && getFinishTime() > (PossibleLevelBestTime+ZeroStarRatingTolerance):
 		$Popup/Control/PopupBackground/AnimationPlayer.play("0StarRating")
+		StarRating = 1
+		if PreviousStarRating < StarRating:
+			setStarRating(StarRating)
+		config.save("user://settings.cfg")
+		
 	else:
 		$Popup/Control/PopupBackground/AnimationPlayer.play("3StarRating")
+		StarRating = 3
+		if PreviousStarRating < StarRating:
+			setStarRating(StarRating)
+		config.save("user://settings.cfg")
 
 
 
@@ -39,3 +67,6 @@ func GrabButtonFocus():
 func _on_ClickSound_finished():
 	get_tree().change_scene_to(scene_to_load)
 	get_tree().paused =false
+
+func setStarRating(StarRating):
+	config.set_value("StarRating",Levelnumber,StarRating)

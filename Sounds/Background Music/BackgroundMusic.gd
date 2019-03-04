1868d5ef857ba3e026bbc629dbae1e75aa165785
ChristianHeusser <none@none>
2019-03-04 10:49:34 +0100

    Fixed: -Background Music now changes between states much better
    --HG--
    branch : NewSound

--- Sounds/Background Music/BackgroundMusic.gd
+++ Sounds/Background Music/BackgroundMusic.gd
@@ -6,7 +6,6 @@ var drumsSilent = true
 var layer2AtFullVolume = false
 var layer2Silent = true
 var layer3AtFullVolume = false
-var layer3Silent = true
 var waitTimeFinished = false
 
 signal drum_beat
@@ -16,74 +15,63 @@ func _ready():
 	$Speedware_Layer1.volume_db = 70
 	$Speedware_Layer2.playing = true
 	$Speedware_Layer2.volume_db = 0
-	$Speedware_Layer3.playing = true
-	$Speedware_Layer3.volume_db = 0
+	$Speedware_Layer3.playing = false
+	$Speedware_Layer3.volume_db = 70
 	$Speedware_Layer2and3_Percussion.playing = true
 	$Speedware_Layer2and3_Percussion.volume_db = 0
 #	$AnimationPlayer.play("OnBeat")
 	pass
 
+func DebugChangeFullspeed():
+	if Input.is_action_just_pressed("jump"):
+		isAtFullspeed = !isAtFullspeed
+
 func _process(delta):
+	
+	DebugChangeFullspeed()
+	
+	$FullspeedTracker.AddNewEntry(isAtFullspeed)
+	
 	if isAtFullspeed:
-		if !layer2AtFullVolume:
-			layer2AtFullVolume = IncreaseVolume($Speedware_Layer2, 70, 0.5)
-			layer2Silent = false
-		elif !layer3AtFullVolume:
-			layer3AtFullVolume = IncreaseVolume($Speedware_Layer3, 70, 0.5)
-			DecreaseVolume($Speedware_Layer2, 0, 0.1)
-			layer3Silent = false
 		if !drumsAtFullVolume:
 			drumsAtFullVolume = IncreaseVolume($Speedware_Layer2and3_Percussion, 60, 5)
 			drumsSilent = false
+		
+		if !layer2AtFullVolume:
+			layer2AtFullVolume = IncreaseVolume($Speedware_Layer2, 70, 0.5)
+			layer2Silent = false
+			
+		elif !layer3AtFullVolume:
+			var playbackPosition = $Speedware_Layer2.get_playback_position()
+			if LayerChangeSpot(playbackPosition, 0.1):
+				$Speedware_Layer2.stop()
+				$Speedware_Layer3.play(playbackPosition)
+				$Speedware_Layer3.volume_db = 70
+				layer3AtFullVolume = true
+	
+			#layer3AtFullVolume = IncreaseVolume($Speedware_Layer3, 70, 0.5)
+			#DecreaseVolume($Speedware_Layer2, 0, 0.1)
+			#layer3Silent = false
+		#if !drumsAtFullVolume:
+		#	drumsAtFullVolume = IncreaseVolume($Speedware_Layer2and3_Percussion, 60, 5)
+		#	drumsSilent = false
 	
 	else:
-		if !layer3Silent:
-			layer3Silent = DecreaseVolume($Speedware_Layer3, 0, 0.1)
-			IncreaseVolume($Speedware_Layer2,70, 0.5)
-			layer3AtFullVolume = false
+		if layer3AtFullVolume && $FullspeedTracker.totalValue < 0:
+			var playbackPosition = $Speedware_Layer3.get_playback_position()
+			if LayerChangeSpot(playbackPosition, 0.1):
+				$Speedware_Layer3.stop()
+				$Speedware_Layer2.play(playbackPosition)
+				$Speedware_Layer2.volume_db = 70
+				layer3AtFullVolume = false
 		elif !layer2Silent:
 			layer2Silent = DecreaseVolume($Speedware_Layer2, 0, 0.1)
 			layer2AtFullVolume = false
 		if !drumsSilent:
 			drumsSilent = DecreaseVolume($Speedware_Layer2and3_Percussion, 0, 0.1)
 			drumsAtFullVolume = false
-	#$FullspeedTracker.AddNewEntry(isAtFullspeed)
-	
-	#if isAtFullspeed:
-	#	if !bassAtFullVolume:
-	#		$Speedware_Basic_Layer_Bass.volume_db += 10
-	#		if $Speedware_Basic_Layer_Bass.volume_db >= 60:
-	#			$Speedware_Basic_Layer_Bass.volume_db = 60
-	#			bassAtFullVolume = true
-	#	elif!drumsAtFullVolume:
-	#		$Speedware_Basic_Layer_Drums.volume_db += 5
-	#		if $Speedware_Basic_Layer_Drums.volume_db >= 65:
-	#			$Speedware_Basic_Layer_Drums.volume_db = 65
-	#			drumsAtFullVolume = true
-	#	if layer2Playing && $FullspeedTracker.totalValue > 0:
-	#		var playbackPosition = $Speedware_Layer_2.get_playback_position()
-	#		if LayerChangeSpot(playbackPosition, 0.1):
-	#			$Speedware_Layer_2.stop()
-	#			$Speedware_Layer_3.play(playbackPosition)
-	#			$Speedware_Layer_3.volume_db = 65
-	#			layer2Playing = false
-	#else:
-	#	if !layer2Playing && $FullspeedTracker.totalValue < 0:
-	#		var playbackPosition = $Speedware_Layer_3.get_playback_position()
-	#		if LayerChangeSpot(playbackPosition, 0.1):
-	#			$Speedware_Layer_3.stop()
-	#			$Speedware_Layer_2.play(playbackPosition)
-	#			$Speedware_Layer_2.volume_db = 70
-	#			layer2Playing = true
-	#	elif $Speedware_Basic_Layer_Drums.volume_db > 0:
-	#		$Speedware_Basic_Layer_Drums.volume_db -= 0.5
-	#		drumsAtFullVolume = false
-	#	elif $Speedware_Basic_Layer_Bass.volume_db > 0:
-	#		$Speedware_Basic_Layer_Bass.volume_db -= 0.5
-	#		bassAtFullVolume = false
-	#		
 
-var spots = [0, 11.852, 23.704]
+var spots = [0, 12, 24]
 #5.926, 17.778,
 
 func LayerChangeSpot(var currentPosition, var wiggle):

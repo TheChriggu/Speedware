298eb753578a4e21122dab97ab7242818957975d
BryanPrintz <none@none>
2019-03-06 18:01:52 +0100

    Added: intro screen

--- Scenes/Intro/LaserLabsIntro.gd
+++ Scenes/Intro/LaserLabsIntro.gd
@@ -7,10 +7,10 @@ func _ready():
 	$AudioStreamPlayer.playing = true
 	config.load("user://settings.cfg")
 
-func _on_AnimationPlayer_animation_finished(StartupAnimation):
-	if not config.has_section_key("Time","00"):
-		get_tree().change_scene("res://Scenes/ButtonLayout.tscn")
-	else:get_tree().change_scene("res://Scenes/Level Selection/LevelSelection.tscn")
+#func _on_AnimationPlayer_animation_finished(StartupAnimation):
+#	if not config.has_section_key("Time","00"):
+#		get_tree().change_scene("res://Scenes/ButtonLayout.tscn")
+#	else:get_tree().change_scene("res://Scenes/Level Selection/LevelSelection.tscn")
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
